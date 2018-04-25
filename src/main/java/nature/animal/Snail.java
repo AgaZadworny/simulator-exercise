7b44c5f10f3638b513333eb5c0ee95bb42d10402
michaelloo35 <michaelloo35@gmail.com>
2018-04-25 02:19:47 +0200

    Breaking ISP and Liskov

---
+++ src/main/java/nature/animal/Snail.java
@@ -0,0 +1,49 @@
+package nature.animal;
+
+import nature.FoodRepository;
+
+import java.lang.invoke.WrongMethodTypeException;
+
+public class Snail implements Animal {
+    private double sizeKg = 3;
+    private boolean hasEaten = false;
+
+    @Override
+    public boolean eat(FoodRepository foodRepository) {
+        double amountToEat = sizeKg / 100;
+        if (foodRepository.consume(amountToEat)) {
+            sizeKg += amountToEat;
+            return hasEaten = true;
+        } else return hasEaten;
+    }
+
+    @Override
+    public void speak() {
+        throw new WrongMethodTypeException();
+    }
+
+    @Override
+    public Poop poop() {
+        if (hasEaten) {
+            hasEaten = false;
+            return new Poop(sizeKg / 150);
+        } else return new Poop(0);
+    }
+
+    @Override
+    public double getSizeKg() {
+        return sizeKg;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return hasEaten;
+    }
+
+    @Override
+    public int compareTo(Animal o) {
+        return Double.compare(sizeKg, o.getSizeKg());
+    }
+
+
+}
